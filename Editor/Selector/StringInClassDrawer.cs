b12bb15132546d27196f59c8ce4198b00fc4bf03
Yuze075 <925581968@qq.com>
2023-05-21 09:55:26 +0800

    add StringInClassAttribute

---
+++ Editor/Selector/StringInClassDrawer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using UnityEditor;
+using UnityEngine;
+
+namespace YuzeToolkit.Attributes.Editor
+{
+    [CustomPropertyDrawer(typeof(StringInClassAttribute))]
+    public class StringInClassDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+
+            // Get targetsType
+            var stringInClassAttribute =
+                (StringInClassAttribute)attribute;
+            var targetType = stringInClassAttribute.TargetType;
+            var matchRule = stringInClassAttribute.MatchRule;
+
+            var listStr = new List<string> { "<empty>" };
+            listStr.AddRange(targetType.GetFields(BindingFlags.Static | BindingFlags.Public)
+                .Select(t => (string)t.GetValue(null))
+                .Where(str => !string.IsNullOrEmpty(str) && Regex.IsMatch(str, matchRule)));
+
+            var selectedIndex = 0;
+            if (listStr.Contains(property.stringValue))
+            {
+                selectedIndex = listStr.IndexOf(property.stringValue);
+            }
+
+            selectedIndex = EditorGUI.Popup(position, label.text,
+                selectedIndex, listStr.ToArray());
+
+            property.stringValue = selectedIndex switch
+            {
+                0 => "",
+                > 0 when selectedIndex < listStr.Count => listStr[selectedIndex],
+                _ => property.stringValue
+            };
+
+            EditorGUI.EndProperty();
+        }
+    }
+}
---
+++ Runtime/Selector/StringInClassAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace YuzeToolkit.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class StringInClassAttribute : PropertyAttribute
+    {
+        public Type TargetType { get; }
+        public string MatchRule { get; }
+
+        public StringInClassAttribute(Type targetType, string matchRule)
+        {
+            TargetType = targetType;
+            MatchRule = matchRule;
+        }
+    }
+}
